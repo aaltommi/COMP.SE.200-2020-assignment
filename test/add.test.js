222aeeef93797b0191df01bed860a29dbea1a7b6
Tommi Aalto <tommi.j.aalto@tuni.fi>
2020-10-14 21:51:10 +0300

    First tests, failing and passing

---
+++ test/add.test.js
@@ -0,0 +1,30 @@
+
+/* import createMathOperation from './.internal/createMathOperation.js'
+
+/**
+ * Adds two numbers.
+ *
+ * @since 3.4.0
+ * @category Math
+ * @param {number} augend The first number in an addition.
+ * @param {number} addend The second number in an addition.
+ * @returns {number} Returns the total.
+ * @example
+ *
+ * add(6, 4)
+ * // => 10
+ 
+const add = createMathOperation((augend, addend) => augend + addend, 0)
+
+export default add */
+
+import add from '../src/add.js';
+import assert from 'assert';
+describe('Test add.js', function () {
+    it('Should return sum of two two numbers', function () {
+      assert.strictEqual(add(4,6), 10);
+    });
+    it('Test faulty input', function () {
+        assert.strictEqual(add('six','four'), 'ten');
+    });
+});

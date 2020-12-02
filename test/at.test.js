1d761721085951fc34804c16c00c218243d709d2
Tommi Aalto <tommi.j.aalto@tuni.fi>
2020-12-02 17:36:21 +0200

    Some tests

--- test/at.test.js
+++ test/at.test.js
@@ -1,1 +1,20 @@
 // test at.js
+
+// Here is a description, taken from function comments, of how this function should work:
+// const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
+
+// at(object, ['a[0].b.c', 'a[1]'])
+// => [3, 4]
+
+import at from '../src/at.js';
+import assert from 'assert';
+describe('Test at.js', function () {
+    const object = { 'a': [{ 'b': { 'c': 3 } }, 4] }
+    const result = [3, 4]
+    it('Should return picked values', function () {
+        assert.strictEqual(at(object, ['a[0].b.c', 'a[1]']), result);
+    });
+    // it('Test faulty input', function () {
+    //     assert.strictEqual(at(object, ['a[0].b.c', 'a[1]']), 'ten');
+    // });
+});
---
+++ test/castArray.test.js
@@ -0,0 +1,1 @@
+// Test castArray.js
